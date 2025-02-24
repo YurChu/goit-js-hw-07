b51acd69060bc804b0782c102b7993096a926a9d
yur_chu <poninka@ukr.net>
2025-02-24 23:36:43 +0200

    hw-js-07-2

--- js/task-1.js
+++ js/task-1.js
@@ -5,6 +5,6 @@ console.log(`number of categories ${itemsOfCategories.length}`);
 itemsOfCategories.forEach(item => {
   const elements = item.querySelectorAll('li');
   const title = item.querySelector('h2').textContent;
-  console.log(`Category: ${itle}`);
+  console.log(`Category: ${title}`);
   console.log(`Elements: ${elements.length}`);
 });
