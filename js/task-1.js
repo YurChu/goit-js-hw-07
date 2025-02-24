f727e26acfe0858f6ed46b7e6d66cac465f3ea88
yur_chu <poninka@ukr.net>
2025-02-24 20:27:54 +0200

    hw-js-07-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,10 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const categories = document.querySelector('#categories');
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const itemsOfCategories = document.querySelectorAll('li.item');
+console.log(`number of categories ${itemsOfCategories.length}`);
+itemsOfCategories.forEach(item => {
+  const elements = item.querySelectorAll('li');
+  const title = item.querySelector('h2').textContent;
+  console.log(`Category: ${itle}`);
+  console.log(`Elements: ${elements.length}`);
+});
--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,11 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-  }
-  padBoth(str) {
-    this.#value = str + this.#value + str;
-  }
-}
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
 
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener('input', () => {
+  const textInput = nameInput.value.trim();
+  if (textInput === '') {
+    nameOutput.textContent = 'Anonymus';
+  } else {
+    nameOutput.textContent = textInput;
+  }
+});
